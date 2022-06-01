c98d1383e178400de01f80603062272d20454358
Yi Wee <yiweelp@gmail.com>
2022-06-01 11:03:51 +0800

    Add maanagereport view and java file

---
+++ src/inventory/system/Controller/ManageReport.java
@@ -0,0 +1,19 @@
+package inventory.system.Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author CB19098 TAN YI WEE
+ */
+
+public class ManageReport {
+
+}
